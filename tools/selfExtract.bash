ef76ae2de6ccf35e8821cf15977ceef6ffcc50eb
Randall White <rlwhite@cfl.rr.com>
2015-02-04 19:44:01 -0500

    echo copyright notice after installation.

--- tools/selfExtract.bash
+++ tools/selfExtract.bash
@@ -23,6 +23,16 @@ castDir="$HOME/cast"
 installDir=""
 isRoot=0
 
+copyrt=$"# The \"Clean And Simple Test\" (CAST) software framework, tools,
+# and documentation are distributed under the terms of the MIT license a
+# copy of which is included with this package (see the file \"LICENSE\"
+# in the CAST poject tree's root directory).  CAST may be used for any
+# purpose, including commercial purposes, at absolutely no cost. No
+# paperwork, no royalties, no GNU-like \"copyleft\" restrictions, either.
+# Just download it and use it.
+#
+# Copyright (c) 2015 Randall Lee White"
+
 testRootUser()
 {
     if [ "root" == "$USER" ]; then
@@ -171,6 +181,7 @@ main()
     makeCast
 
     setLinksIfRoot
+    echo "$copyrt"
 }
 
 main "$@"
