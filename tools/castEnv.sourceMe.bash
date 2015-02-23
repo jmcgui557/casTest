1fc273cb8fd47bc991b8ab12bc34f63ae77449c5
Randall White <rlwhite@cfl.rr.com>
2015-02-22 23:08:29 -0500

    - Added tools/castEnv.sourceMe.bash for setting environment variables - primarily for those installing via github.
    - Tweaked README to explain how to use tools/castEnv.sourceMe.bash

---
+++ tools/castEnv.sourceMe.bash
@@ -0,0 +1,19 @@
+# The "Clean And Simple Test" (CAST) software framework, tools, and
+# documentation are distributed under the terms of the MIT license a
+# copy of which is included with this package (see the file "LICENSE"
+# in the CAS poject tree's root directory).  CAS may be used for any
+# purpose, including commercial purposes, at absolutely no cost. No
+# paperwork, no royalties, no GNU-like "copyleft" restrictions, either.
+# Just download it and use it.
+# 
+# Copyright (c) 2015 Randall Lee White
+
+#Copy this file to your home directory and edit
+#the following line to define CAST_DIR as the
+#casTest project root.  Then, source this file 
+#before using casTest or doing any development
+#on the casTest project.
+export CAST_DIR=
+. $CAST_DIR/cast_env.bash
+
+
